0037f4b19fa0ec8eca96ce8a4ae32c94017d5b3f
pnadolny <Paul.Nadolny@transplace.com>
2020-01-26 10:04:02 -0600

    Build test better

--- reverse_test.go
+++ reverse_test.go
@@ -1,10 +1,27 @@
 package algorithms_in_go
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestReverse(t *testing.T) {
 
-	Reverse("paul")
+	tests := []struct {
+		word string
+		want string
+	}{
+		{"paul", "luap"},
+	}
+
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%v", tc.word), func(t *testing.T) {
+			got := Reverse(tc.word)
+			if got != tc.want {
+				t.Fatalf("Reverse = %v; want %v", got, tc.want)
+			}
+		})
+	}
 
 }
 
