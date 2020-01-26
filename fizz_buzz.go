93eb5f876d832d934f3b01d38bae58b63068adb8
pnadolny <Paul.Nadolny@transplace.com>
2020-01-26 10:27:40 -0600

    FizzBuzz

---
+++ fizz_buzz.go
@@ -0,0 +1,30 @@
+package algorithms_in_go
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// FizzBuzz will print out all of the numbers
+// from 1 to N replacing any divisible by 3
+// with "Fizz", and divisible by 5 with "Buzz",
+// and any divisible by both with "Fizz Buzz".
+//
+
+func FizzBuzz(n int) {
+	var sb strings.Builder
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 {
+			sb.WriteString("Fizz")
+		} else if i%5 == 0 {
+			sb.WriteString("Buzz")
+		} else {
+			sb.WriteString(strconv.Itoa(i))
+		}
+		sb.WriteString(",")
+	}
+	finalString := strings.TrimSuffix(sb.String(), ",")
+	fmt.Println(finalString)
+
+}
---
+++ fizz_buzz_test.go
@@ -0,0 +1,9 @@
+package algorithms_in_go
+
+import "testing"
+
+func TestFizzBuzz(t *testing.T) {
+
+	FizzBuzz(4)
+
+}
