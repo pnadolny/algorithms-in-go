abda1c497adbb7f9e71f1506f803ab5b544024bb
pnadolny <Paul.Nadolny@transplace.com>
2020-01-26 10:06:59 -0600

    Build test better

--- reverse.go
+++ reverse.go
@@ -1,26 +1,23 @@
 package algorithms_in_go
 
 import (
-	"fmt"
 	"strings"
 )
 
 func Reverse(word string) string {
 	var res = ""
-	for i:= len(word) -1; i>=0; i-- {
+	for i := len(word) - 1; i >= 0; i-- {
 		res = res + string(word[i])
 	}
-	fmt.Print(res)
 	return res
 
 }
 
 func ReverseWithStringBuilder(word string) string {
 	var s strings.Builder
-	for i:= len(word) -1; i>=0; i-- {
+	for i := len(word) - 1; i >= 0; i-- {
 		s.WriteByte(word[i])
 	}
-	fmt.Println(s.String())
 	return s.String()
 
 }
